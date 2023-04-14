5955cd9c6ad7fd08fe50afd879c6319a1a8c1a5c
백은지 <baeg-eunji@baeg-eunjiui-MacBookAir.local>
2023-04-14 14:14:30 +0900

    commit

---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Products from "../components/ui/Products";
+
+export default function AllProducts() {
+  return <Products />;
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../components/Banner";
+import Products from "../components/ui/Products";
+
+export default function Home() {
+  return (
+    <>
+      <Banner />
+      <Products />
+    </>
+  );
+}
